08f665b5da381c9820a11040b53ae0fea35bfb1e
CarlosGonzalezN <carlogonzalez@alumno.unlam.edu.ar>
2022-09-22 00:37:11 -0300

    fix: router

--- components/index.jsx
+++ components/index.jsx
@@ -1,5 +1,5 @@
 export { default as Category } from "./categori/index";
-export { default as Promotion } from "./Promotion/index";
+export { default as Promotion } from "./promotion/index";
 export { default as Products } from "./products/index";
-export { default as Serch } from "./Serch/index";
+export { default as Serch } from "./serch/index";
 export { default as Carrousel } from "./carrousel/index";
